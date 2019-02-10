983e06c4883fcc780bcfecc1d1ba1bab6cf2610e
mateusgomesc7 <mateusgomesc7@gmail.com>
2019-02-10 10:38:22 -0300

    Comentando códigos

--- iris-crud/json/tempo-valor2.php
+++ iris-crud/json/tempo-valor2.php
@@ -1,6 +1,7 @@
 <?php
 require_once('../read-iris.php');
 
+//Cabeçário para configurar o arquivo php em json
 header("Content-type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 
--- iris-crud/json/tempo-valor3.php
+++ iris-crud/json/tempo-valor3.php
@@ -1,6 +1,9 @@
 <?php
 require_once('../read-iris.php');
 
+//Cabeçário para configurar o arquivo php em json
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
 
 //Transofrma arrya php em json
 var_dump(json_encode(tempoValor3()));
