d2b150d7afc64d3c12c10e2568271f133f639a52
mateusgomesc7 <mateusgomesc7@gmail.com>
2019-02-12 12:16:41 -0300

    ajeitando informações na busca dos dados da ESP

--- iris-crud/salvar.php
+++ iris-crud/salvar.php
@@ -6,18 +6,18 @@
 
 //parametros que serão passados atraves de de requisição GET
     //variavel que armazenará o sinal dos sensores
-    $valor1 = $_GET['valor1'];
-    $valor2 = $_GET['valor2'];
-    $valor3 = $_GET['valor3'];
-    $sql = "INSERT INTO valores (valor1,valor2,valor3, tempo) VALUES (:valor1,:valor2,:valor3, NOW())";//query slq de conexão
+    $temperatura = $_GET['temperatura'];
+    $umidade = $_GET['umidade'];
+    $reservatorio = $_GET['reservatorio'];
+    $sql = "INSERT INTO valores (temperatura,umidade,reservatorio, tempo) VALUES (:temperatura,:umidade,:reservatorio, NOW())";//query slq de conexão
 
     //procedimento de preparação dos dados para inserção evitando qualquer tipo de ataque
     //stmt (statement) serve para preparar os dados para inserção
     $stmt = $PDO->prepare($sql);
     //estabelece de ondem virão os valores de :sensor1 e :sensor2
-    $stmt->bindParam(':valor1',$valor1);
-    $stmt->bindParam(':valor2',$valor2);
-    $stmt->bindParam(':valor3',$valor3);
+    $stmt->bindParam(':temperatura',$temperatura);
+    $stmt->bindParam(':umidade',$umidade);
+    $stmt->bindParam(':reservatorio',$reservatorio);
 
     if($stmt->execute()){
         echo "Dados_Salvos_Com_Sucesso";
